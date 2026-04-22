b7b887789ada113b3fd06be447e62ca6557f3ba2
Jangboo Lee <github@jangboolee.com>
2026-04-22 05:30:57 +0200

    Ruff-format test file

--- genai-graphrag-python/test_environment.py
+++ genai-graphrag-python/test_environment.py
@@ -1,13 +1,14 @@
-# This will test the environment to ensure that the .env file is set up 
+# This will test the environment to ensure that the .env file is set up
 # correctly and that the OpenAI and Neo4j connections are working.
 import os
 import unittest
 
-from dotenv import load_dotenv, find_dotenv
+from dotenv import find_dotenv, load_dotenv
+
 load_dotenv()
 
-class TestEnvironment(unittest.TestCase):
 
+class TestEnvironment(unittest.TestCase):
     skip_env_variable_tests = True
     skip_openai_test = True
     skip_neo4j_test = True
@@ -20,41 +21,42 @@ class TestEnvironment(unittest.TestCase):
 
     def env_variable_exists(self, variable_name):
         self.assertIsNotNone(
-            os.getenv(variable_name),
-            f"{variable_name} not found in .env file")
+            os.getenv(variable_name), f"{variable_name} not found in .env file"
+        )
 
     def test_openai_variables(self):
         if TestEnvironment.skip_env_variable_tests:
             self.skipTest("Skipping OpenAI env variable test")
 
-        self.env_variable_exists('OPENAI_API_KEY')
+        self.env_variable_exists("OPENAI_API_KEY")
         TestEnvironment.skip_openai_test = False
 
     def test_neo4j_variables(self):
         if TestEnvironment.skip_env_variable_tests:
             self.skipTest("Skipping Neo4j env variables test")
 
-        self.env_variable_exists('NEO4J_URI')
-        self.env_variable_exists('NEO4J_USERNAME')
-        self.env_variable_exists('NEO4J_PASSWORD')
-        self.env_variable_exists('NEO4J_DATABASE')
+        self.env_variable_exists("NEO4J_URI")
+        self.env_variable_exists("NEO4J_USERNAME")
+        self.env_variable_exists("NEO4J_PASSWORD")
+        self.env_variable_exists("NEO4J_DATABASE")
         TestEnvironment.skip_neo4j_test = False
 
     def test_openai_connection(self):
         if TestEnvironment.skip_openai_test:
             self.skipTest("Skipping OpenAI test")
 
-        from openai import OpenAI, AuthenticationError
+        from openai import AuthenticationError, OpenAI
 
         llm = OpenAI()
-        
+
         try:
             models = llm.models.list()
-        except AuthenticationError as e:
+        except AuthenticationError:
             models = None
         self.assertIsNotNone(
             models,
-            "OpenAI connection failed. Check the OPENAI_API_KEY key in .env file.")
+            "OpenAI connection failed. Check the OPENAI_API_KEY key in .env file.",
+        )
 
     def test_neo4j_connection(self):
 
@@ -67,39 +69,38 @@ class TestEnvironment(unittest.TestCase):
         from neo4j import GraphDatabase
 
         driver = GraphDatabase.driver(
-            os.getenv('NEO4J_URI'),
-            auth=(os.getenv('NEO4J_USERNAME'), 
-                  os.getenv('NEO4J_PASSWORD'))
+            os.getenv("NEO4J_URI"),
+            auth=(os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD")),
         )
         try:
             driver.verify_connectivity()
             try:
-                driver.execute_query("RETURN true", database_=os.getenv('NEO4J_DATABASE'))
+                driver.execute_query(
+                    "RETURN true", database_=os.getenv("NEO4J_DATABASE")
+                )
                 connected = True
 
-            except Exception as e:
+            except Exception:
                 msg = "Neo4j database query failed. Check the NEO4J_DATABASE value in .env file."
-                
-        except Exception as e:
+
+        except Exception:
             msg = "Neo4j verify connection failed. Check the NEO4J_URI, NEO4J_USERNAME, and NEO4J_PASSWORD values in .env file."
-            
+
         driver.close()
 
-        self.assertTrue(
-            connected,
-            msg
-            )
-        
+        self.assertTrue(connected, msg)
+
+
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest(TestEnvironment('test_env_file_exists'))
-    suite.addTest(TestEnvironment('test_openai_variables'))
-    suite.addTest(TestEnvironment('test_neo4j_variables'))
-    suite.addTest(TestEnvironment('test_openai_connection'))
-    suite.addTest(TestEnvironment('test_neo4j_connection'))
+    suite.addTest(TestEnvironment("test_env_file_exists"))
+    suite.addTest(TestEnvironment("test_openai_variables"))
+    suite.addTest(TestEnvironment("test_neo4j_variables"))
+    suite.addTest(TestEnvironment("test_openai_connection"))
+    suite.addTest(TestEnvironment("test_neo4j_connection"))
     return suite
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     runner = unittest.TextTestRunner()
     runner.run(suite())
-    
