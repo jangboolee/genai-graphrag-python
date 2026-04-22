3b10dc944e873b2daea3a348191500a70dacfb83
Jangboo Lee <github@jangboolee.com>
2026-04-22 05:31:27 +0200

    Add custom chunk sizes

--- genai-graphrag-python/kg_builder.py
+++ genai-graphrag-python/kg_builder.py
@@ -1,37 +1,38 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
 import asyncio
+import os
 
+from dotenv import load_dotenv
 from neo4j import GraphDatabase
-from neo4j_graphrag.llm import OpenAILLM
 from neo4j_graphrag.embeddings import OpenAIEmbeddings
+from neo4j_graphrag.experimental.components.text_splitters.fixed_size_splitter import (
+    FixedSizeSplitter,
+)
 from neo4j_graphrag.experimental.pipeline.kg_builder import SimpleKGPipeline
+from neo4j_graphrag.llm import OpenAILLM
+
+load_dotenv()
 
 neo4j_driver = GraphDatabase.driver(
     os.getenv("NEO4J_URI"),
-    auth=(os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD"))
+    auth=(os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD")),
 )
 neo4j_driver.verify_connectivity()
 
 llm = OpenAILLM(
-    model_name="gpt-5-nano",
-    model_params={
-        "reasoning_effort": "minimal"
-    }
+    model_name="gpt-5-nano", model_params={"reasoning_effort": "minimal"}
 )
 
-embedder = OpenAIEmbeddings(
-    model="text-embedding-ada-002"
-)
+embedder = OpenAIEmbeddings(model="text-embedding-ada-002")
+
+text_splitter = FixedSizeSplitter(chunk_size=500, chunk_overlap=100)
 
 kg_builder = SimpleKGPipeline(
     llm=llm,
-    driver=neo4j_driver, 
-    neo4j_database=os.getenv("NEO4J_DATABASE"), 
-    embedder=embedder, 
+    driver=neo4j_driver,
+    neo4j_database=os.getenv("NEO4J_DATABASE"),
+    embedder=embedder,
     from_pdf=True,
+    text_splitter=text_splitter,
 )
 
 pdf_file = "./genai-graphrag-python/data/genai-fundamentals_1-generative-ai_1-what-is-genai.pdf"
