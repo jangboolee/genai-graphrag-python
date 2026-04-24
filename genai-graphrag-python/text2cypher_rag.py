208069f2d1810209792ec93f7eb9afd37c93cfcb
Jangboo Lee <github@jangboolee.com>
2026-04-24 07:01:31 +0200

    Add text 2 cypher pipeline

--- genai-graphrag-python/text2cypher_rag.py
+++ genai-graphrag-python/text2cypher_rag.py
@@ -1,28 +1,29 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
+from dotenv import load_dotenv
 from neo4j import GraphDatabase
-from neo4j_graphrag.llm import OpenAILLM
 from neo4j_graphrag.generation import GraphRAG
+from neo4j_graphrag.llm import OpenAILLM
 from neo4j_graphrag.retrievers import Text2CypherRetriever
+
+load_dotenv()
 
 # Connect to Neo4j database
 driver = GraphDatabase.driver(
-    os.getenv("NEO4J_URI"), 
-    auth=(
-        os.getenv("NEO4J_USERNAME"), 
-        os.getenv("NEO4J_PASSWORD")
-    )
+    os.getenv("NEO4J_URI"),
+    auth=(os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD")),
 )
 
-llm = OpenAILLM(
-    model_name="gpt-5.2"
-)
+llm = OpenAILLM(model_name="gpt-5.2")
 
 # Cypher examples as input/query pairs
 examples = [
-    "USER INPUT: 'Find a node with the name $name?' QUERY: MATCH (node) WHERE toLower(node.name) CONTAINS toLower($name) RETURN node.name AS name, labels(node) AS labels",
+    """
+    USER INPUT: 'Find a node with the name $name?' 
+    QUERY: MATCH (node) 
+        WHERE toLower(node.name) CONTAINS toLower($name) 
+        RETURN node.name AS name, labels(node) AS labels
+    """,
 ]
 
 # Build the retriever
@@ -33,17 +34,11 @@ retriever = Text2CypherRetriever(
     examples=examples,
 )
 
-rag = GraphRAG(
-    retriever=retriever, 
-    llm=llm
-)
+rag = GraphRAG(retriever=retriever, llm=llm)
 
 query_text = "How many technologies are mentioned in the knowledge graph?"
 
-response = rag.search(
-    query_text=query_text,
-    return_context=True
-    )
+response = rag.search(query_text=query_text, return_context=True)
 
 print(response.answer)
 print("CYPHER :", response.retriever_result.metadata["cypher"])
