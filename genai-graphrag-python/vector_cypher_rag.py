3d85f04eaa860ff68c9b674e8a7b0405122cfe57
Jangboo Lee <github@jangboolee.com>
2026-04-24 06:58:09 +0200

    Add vector to cypher pipelie

--- genai-graphrag-python/vector_cypher_rag.py
+++ genai-graphrag-python/vector_cypher_rag.py
@@ -1,20 +1,18 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
+from dotenv import load_dotenv
 from neo4j import GraphDatabase
 from neo4j_graphrag.embeddings.openai import OpenAIEmbeddings
-from neo4j_graphrag.retrievers import VectorCypherRetriever
-from neo4j_graphrag.llm import OpenAILLM
 from neo4j_graphrag.generation import GraphRAG
+from neo4j_graphrag.llm import OpenAILLM
+from neo4j_graphrag.retrievers import VectorCypherRetriever
+
+load_dotenv()
 
 # Connect to Neo4j database
 driver = GraphDatabase.driver(
-    os.getenv("NEO4J_URI"), 
-    auth=(
-        os.getenv("NEO4J_USERNAME"), 
-        os.getenv("NEO4J_PASSWORD")
-    )
+    os.getenv("NEO4J_URI"),
+    auth=(os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD")),
 )
 
 # Create embedder
@@ -22,7 +20,25 @@ embedder = OpenAIEmbeddings(model="text-embedding-ada-002")
 
 # Define retrieval query
 retrieval_query = """
-RETURN node.text as text, score
+MATCH (node)-[:FROM_DOCUMENT]->(d)-[:PDF_OF]->(lesson)
+RETURN
+    node.text as text, score,
+    lesson.url as lesson_url,
+    collect { 
+        MATCH (node)<-[:FROM_CHUNK]-(entity)-[r]->(other)-[:FROM_CHUNK]->()
+        WITH toStringList([
+            labels(entity)[2], 
+            entity.name, 
+            entity.type, 
+            entity.description, 
+            type(r), 
+            labels(other)[2], 
+            other.name, 
+            other.type, 
+            other.description
+            ]) as values
+        RETURN reduce(acc = "", item in values | acc || coalesce(item || ' ', ''))
+    } as associated_entities
 """
 
 # Create retriever
@@ -35,9 +51,7 @@ retriever = VectorCypherRetriever(
 )
 
 #  Create the LLM
-llm = OpenAILLM(
-    model_name="gpt-5.2"
-)
+llm = OpenAILLM(model_name="gpt-5.2")
 
 # Create GraphRAG pipeline
 rag = GraphRAG(retriever=retriever, llm=llm)
@@ -46,9 +60,7 @@ rag = GraphRAG(retriever=retriever, llm=llm)
 query_text = "What is a knowledge graphs and where can I learn about them?"
 
 response = rag.search(
-    query_text=query_text, 
-    retriever_config={"top_k": 5},
-    return_context=True
+    query_text=query_text, retriever_config={"top_k": 5}, return_context=True
 )
 
 print(response.answer)
